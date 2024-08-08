4803047a472716f7c20508f492b4bab063118616
sundarshahi <shahithakurisundar@gmail.com>
2024-08-08 22:29:07 +0545

    chore: initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ImageProcessingLoader } from "./components";
+
+function App() {
+  return (
+    <div className="p-4 m-4 flex items-center justify-center">
+      <ImageProcessingLoader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ImageProcessingLoader/ImageProcessingLoadeer.tsx
@@ -0,0 +1,5 @@
+function ImageProcessingLoader() {
+  return <div>Loading...</div>;
+}
+
+export default ImageProcessingLoader;
